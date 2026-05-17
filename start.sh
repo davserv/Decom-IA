9bc3bc5f0f6e6e7c6efba93b25cd351c994fda98
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2026-05-17 12:41:02 -0300

    1.1
    1.1

--- start.sh
+++ start.sh
@@ -19,6 +19,8 @@ git clone https://github.com/davserv/Decom-IA.git
 
 cd Decom-IA
 
+clear
+
 echo "EDITAR O ARQUIVO chat.sh PARA CONFIGURAR SUA API KEY, URL E MODELO"
 
 echo "EXECUTE O COMANDO ABAIXO PARA INICIAR O CHAT"
