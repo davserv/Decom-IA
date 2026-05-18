a1c2badfc9df875042ca23a1bca0e7c6534f57df
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2026-05-17 22:07:35 -0300

    1.1
    1.1

---
+++ github/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+echo "Instalando..."
+
+cat << "EOF"
+
+████  █████  ███   ███  █   █    ███  ███  
+█   █ █     █     █   █ ██ ██     █  █   █ 
+█   █ ████  █     █   █ █ █ █     █  █████ 
+█   █ █     █     █   █ █   █     █  █   █ 
+████  █████  ███   ███  █   █    ███ █   █
+
+            ISTALLER DECOM IA
+
+EOF
+
+git clone https://github.com/davserv/Decom-IA.git
+
+sleep 2
+
+cd Decom-IA/github
+
+sleep 5
+clear
+
+echo "ENTRANDO NA PASTA DO PROJETO..."
+echo "'cd Decom-IA/github'"
+
+echo "EDITAR O ARQUIVO chat.sh PARA CONFIGURAR SUA API KEY, URL E MODELO"
+
+echo "EXECUTE O COMANDO ABAIXO PARA INICIAR O CHAT"
+echo ""
+echo "chmod +x chat.sh && ./chat.sh"
+echo ""
